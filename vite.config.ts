22283ee0024b73d7c380850bbebd5acfe4b1da57
edgar.treischl <edgar.treischl@isb.bayern.de>
2026-04-12 16:14:31 +0200

    deploy test mfe, baby

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,40 @@ import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
+  // IMPORTANT for GitHub Pages deployment
+  base: '/test-mfe/',
+
   plugins: [
     react(),
     federation({
-      name: 'hr',
+      name: 'test-mfe',
       filename: 'remoteEntry.js',
       exposes: {
         './HRApp': './src/App.tsx',
       },
-      shared: ['react', 'react-dom'],
+
+      // POC fix for TS issue
+      shared: {
+        react: { singleton: true },
+        'react-dom': { singleton: true },
+      } as any,
     }),
   ],
+
   server: {
     host: true,
     port: 5176,
     cors: true,
   },
+
+  preview: {
+    host: true,
+    port: 5176,
+  },
+
   build: {
     target: 'esnext',
     cssCodeSplit: false,
+    minify: false,
   },
 });
